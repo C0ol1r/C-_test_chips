76a81221ea6c3059725843c9b6a2199062685c00
C0ol1r <168990666+C0ol1r@users.noreply.github.com>
2024-05-26 23:11:26 +0600

    Add files via upload

--- chip_test.cs
+++ chip_test.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 class MainClass
 {
@@ -10,12 +10,13 @@ class MainClass
             new int[] { 1, 2, 3 },
             new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
             new int[] { 0, 1, 1, 1, 2, 1, 1, 1, 1, 1 },
-            new int[] { 0, 10,0 ,8, 3, 10, 7, 0, 9, 3 }
+            new int[] { 0, 10,0 ,8, 3, 10, 7, 0, 9, 3 },
+            new int[] { 13, 8, 28, 21, 30, 6, 13, 27, 23, 1}
         };
-        
+
         foreach (int[] chips in setsOfChips)
         {
-            foreach(int i in chips)
+            foreach (int i in chips)
             {
                 Console.Write(i);
             }
@@ -36,13 +37,13 @@ class MainClass
         {
             //foreach (int i in chips)
             //{
-              //  Console.Write(i);
+            //  Console.Write(i);
             //}
             //Console.WriteLine();
             indices_small = FindIndicesWithLessThanAverage(chips);
             indices_up = FindIndicesWithUpThanAverage(chips);
             way = FindTheMostWantedMove(indices_small, indices_up, chips);
-            Move(way,chips);
+            Move(way, chips);
             moves++;
         }
 
@@ -57,33 +58,11 @@ class MainClass
             foreach (int maxIndex in indices_up)
             {
                 listOfLists.Add(CalculateDistance(minIndex, maxIndex, chips.Length));
-            }
-        }
-        int max = int.MinValue;
-        int maxindex = 0;
-        for (int i=0; i< listOfLists.Count;i++)
-        {
-            if (listOfLists[i][1] > max)
-            {
-                max = listOfLists[i][1];
-                maxindex = i;
-            }
-
-        }
-
-        int bias = maxindex / indices_up.Count;
-        int min = int.MaxValue;
-        int minIndex_ = 0;
-        for (int i=0+bias * indices_up.Count;i< bias * indices_up.Count+1;i++)
-        {
-            if (listOfLists[i][1] < min)
-            {
-                min = listOfLists[i][1];
-                minIndex_ = i;
+                //Console.WriteLine("" + maxIndex + "" + minIndex);
             }
         }
 
-        return [listOfLists[minIndex_][0], listOfLists[minIndex_][2]];
+        return [listOfLists[0][0], listOfLists[0][2]];
     }
     static int Neighbour(int L, int index)
     {
@@ -154,18 +133,18 @@ class MainClass
         if (directDistance < wrapAroundDistance)
         {
             direction = (maxIndex > minIndex) ? -1 : 1;
-            return [direction,directDistance, maxIndex, minIndex];
+            return [direction, directDistance, maxIndex, minIndex];
         }
         else
         {
             direction = (maxIndex > minIndex) ? 1 : -1;
-            return [direction,wrapAroundDistance,maxIndex,minIndex];
+            return [direction, wrapAroundDistance, maxIndex, minIndex];
         }
     }
     static int Move(List<int> way, int[] chips)
     {
         chips[Neighbour(chips.Length, way[1] + way[0])]++;
-        chips[Neighbour(chips.Length,way[1])]--;
+        chips[Neighbour(chips.Length, way[1])]--;
         return 0;
     }
 }
